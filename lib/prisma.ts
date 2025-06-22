ca1687239f87dad7f1e02de209ee0889acd64ffb
Blankaert <blankaertlouis@outlook.com>
2025-06-22 16:55:19 +0200

    Fix: Correction de l'importation de Prisma pour la version 6.x

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,12 +1,12 @@
-import { PrismaClient } from '@prisma/client';
+import * as prismaClient from '@prisma/client';
 
 // PrismaClient est attaché à l'objet global en développement pour éviter
 // d'épuiser la limite de connexions à la base de données pendant le hot-reloading
 declare global {
-  var prisma: PrismaClient | undefined;
+  var prisma: typeof prismaClient | undefined;
 }
 
-const prisma = global.prisma || new PrismaClient();
+const prisma = global.prisma || prismaClient;
 
 if (process.env.NODE_ENV !== 'production') {
   global.prisma = prisma;
