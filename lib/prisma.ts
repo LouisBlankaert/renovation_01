bf426a415c5801fb579baf8909c4be60875138ce
Blankaert <blankaertlouis@outlook.com>
2025-06-22 20:03:09 +0200

    fix: Simplification du client Prisma sans dépendance à l'extension Accelerate

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -2,21 +2,7 @@ import { PrismaClient } from '@prisma/client';
 
 // Déclaration globale pour le singleton
 declare global {
-  var prismaClient: any;
-}
-
-// Fonction pour créer un client Prisma
-// Essaie d'utiliser Accelerate si disponible, sinon utilise le client standard
-function getPrismaClient(): PrismaClient {
-  try {
-    // Import dynamique pour éviter les erreurs si le module n'est pas disponible
-    // eslint-disable-next-line @typescript-eslint/no-require-imports
-    const { withAccelerate } = require('@prisma/extension-accelerate');
-    return new PrismaClient().$extends(withAccelerate()) as unknown as PrismaClient;
-  } catch (error) {
-    console.log('Prisma Accelerate non disponible, utilisation du client standard');
-    return new PrismaClient();
-  }
+  var prismaClient: PrismaClient | undefined;
 }
 
 // Initialisation du client Prisma en fonction de l'environnement
@@ -24,11 +10,11 @@ let client: PrismaClient;
 
 if (process.env.NODE_ENV === 'production') {
   // En production, créer une nouvelle instance
-  client = getPrismaClient();
+  client = new PrismaClient();
 } else {
   // En développement, réutiliser l'instance existante si disponible
   if (!global.prismaClient) {
-    global.prismaClient = getPrismaClient();
+    global.prismaClient = new PrismaClient();
   }
   client = global.prismaClient;
 }
